85ce822e292fbe480c51cbfde9a88d64ce83a97f
Ilya Shmyryov <119329448+ilyash0@users.noreply.github.com>
2025-05-17 01:00:02 +0500

    Add function to ask nickname

--- game/views.py
+++ game/views.py
@@ -1,6 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import TemplateView
 
 
 class MainMenuView(TemplateView):
     template_name = "game/main_menu.html"
+
+    def get_context_data(self, **kwargs):
+        ctx = super().get_context_data(**kwargs)
+        ctx['nickname'] = self.request.COOKIES.get('nick', '')
+        return ctx
