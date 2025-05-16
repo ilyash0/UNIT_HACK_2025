6876ac0fcb0101cd2c3b3b2ad14886be266bb494
Ilya Shmyryov <119329448+ilyash0@users.noreply.github.com>
2025-05-17 00:44:45 +0500

    Add STATIC_ROOT

--- UNIT_HACK_2025/settings.py
+++ UNIT_HACK_2025/settings.py
@@ -135,6 +135,7 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
    path.join(BASE_DIR, "static"),
 ]
+STATIC_ROOT = path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
