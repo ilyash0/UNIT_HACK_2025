aeeaf82c29424df458191d57534f9ff63aef0fed
Ilya Shmyryov <119329448+ilyash0@users.noreply.github.com>
2025-05-17 16:39:44 +0500

    Fix

--- UNIT_HACK_2025/asgi.py
+++ UNIT_HACK_2025/asgi.py
@@ -13,7 +13,7 @@ from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 
-import game.urls
+from game.urls import websocket_urlpatterns
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'UNIT_HACK_2025.settings')
 
@@ -21,9 +21,9 @@ django_asgi_app = get_asgi_application()
 
 application = ProtocolTypeRouter({
     'http': django_asgi_app,
-    # 'websocket': AuthMiddlewareStack(
-    #     URLRouter(
-    #         game.urls.websocket_urlpatterns
-    #     )
-    # ),
+    'websocket': AuthMiddlewareStack(
+        URLRouter(
+            websocket_urlpatterns
+        )
+    ),
 })
