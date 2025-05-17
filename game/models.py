9363663ac2dac4c5e44c40156dc74e345ca6316d
Ilya Shmyryov <119329448+ilyash0@users.noreply.github.com>
2025-05-17 14:24:57 +0500

    Add Player model

--- game/models.py
+++ game/models.py
@@ -1,6 +1,15 @@
 from django.db import models
 
 
+class Player(models.Model):
+    telegram_id = models.BigIntegerField(unique=True)
+    username = models.CharField(max_length=50)
+    joined_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.username
+
+
 class Prompt(models.Model):
     phrase = models.TextField()
 
