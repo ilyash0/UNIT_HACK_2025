933b77be50b26a427e7c42b252fc2e52aacdb4cc
Ilya Shmyryov <119329448+ilyash0@users.noreply.github.com>
2025-05-24 18:27:28 +0500

    Fix serializers.py

--- game/serializers.py
+++ game/serializers.py
@@ -2,19 +2,19 @@ from rest_framework import serializers
 
 
 class RegisterPlayerInputSerializer(serializers.Serializer):
-    type = serializers.CharField(default='register_player', required=True, allow_blank=False)
+    type = serializers.CharField(default='register_player', allow_blank=False)
     telegram_id = serializers.IntegerField(required=True)
     username = serializers.CharField(required=False, allow_blank=True)
 
 
 class SendPlayerAnswerInputSerializer(serializers.Serializer):
-    type = serializers.CharField(default='send_player_answer', required=True, allow_blank=False)
+    type = serializers.CharField(default='send_player_answer', allow_blank=False)
     telegram_id = serializers.IntegerField(required=True)
     answer = serializers.CharField(required=True, allow_blank=False)
 
 
 class SendPlayerVoteInputSerializer(serializers.Serializer):
-    type = serializers.CharField(default='send_player_vote', required=True, allow_blank=False)
+    type = serializers.CharField(default='send_player_vote', allow_blank=False)
     voter_id = serializers.IntegerField(required=True)
     candidate_id = serializers.IntegerField(required=True)
 
